9ac527c718afbf709b30f9717242ce4334f37c27
rohan <rohankhurana77@gmail.com>
2023-03-08 20:21:57 +0530

    added duty ratio logic

--- backend/views.py
+++ backend/views.py
@@ -16,4 +16,43 @@ class ReadingDetail(generics.RetrieveUpdateDestroyAPIView):
 
 class DutyRatio(APIView):
     def get(self, request, format=None):
-        return Response("55")
+        last_two = Reading.objects.all().order_by('-id')[:2]
+        serializer = ReadingSerializer(last_two, many=True)
+
+        # VAprev = float(serializer.data[1]['v_oc'])
+        # IAprev = float(serializer.data[1]['i_sc'])
+
+        VAprev = 2.0
+        IAprev = 1.0
+        PAprev = IAprev * VAprev
+
+        Dprev = 1.5
+
+        D = Dprev
+        # VA = float(serializer.data[0]['v_oc'])
+        # IA = float(serializer.data[0]['i_sc'])
+
+        VA = 1.0
+        IA = 4.0
+        PA = VA 
+
+        DeltaVA = VA - VAprev
+        DeltaPA = PA - PAprev
+
+        if DeltaPA>0:
+            if DeltaVA>0:
+                D = Dprev - 0.0001
+            elif DeltaVA<0:
+                D = Dprev + 0.0001
+        elif DeltaPA<0:
+            if DeltaVA>0:
+                D = Dprev + 0.0001
+            elif DeltaVA<0:
+                D = Dprev - 0.0001
+        
+        if D>0.9:
+            D=0.9
+        elif D<0:
+            D=0
+        
+        return Response(D)
